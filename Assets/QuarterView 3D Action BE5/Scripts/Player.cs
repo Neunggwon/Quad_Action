3d926faffb2e46ec2dbe0eaf259f6e2b1888a4d2
Neunggwon <104968524+Neunggwon@users.noreply.github.com>
2023-05-04 15:59:32 +0900

    Quad_Action-1.0.0
    Quad_Action-1.0.0

---
+++ Assets/QuarterView 3D Action BE5/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    float hAxis;
+    float vAxis;
+    bool wDown;
+
+    Vector3 moveVec;
+
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        wDown = Input.GetButton("Walk");
+
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+  
+        /*transform.position += moveVec * speed * Time.deltaTime;*/
+        transform.position += moveVec * speed * (wDown ? 0.3f : 1f) *Time.deltaTime;
+
+        anim.SetBool("IsRun", moveVec != Vector3.zero);
+        anim.SetBool("IsWalk", wDown);
+
+        transform.LookAt(transform.position + moveVec);
+    }
+}
