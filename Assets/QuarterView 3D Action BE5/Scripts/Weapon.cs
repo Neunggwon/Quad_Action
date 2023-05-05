b03ac4afb33fecbaec00ae05f9fc509562285726
Neunggwon <104968524+Neunggwon@users.noreply.github.com>
2023-05-06 03:54:22 +0900

    Qaud Action-1.0.0
    Qaud Action-1.0.0

---
+++ Assets/QuarterView 3D Action BE5/Scripts/Weapon.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public enum Type { melee, Range };
+    public Type type;
+    public int damage;
+    public float rate;
+    public BoxCollider meleeArea;
+    public TrailRenderer trailEffect;
+
+    public void Use()
+    {
+        if (type == Type.melee)
+        {
+            StartCoroutine("Swing");
+        }
+    }
+
+    IEnumerator Swing()
+    {
+        yield return new WaitForSeconds(0.1f);
+        meleeArea.enabled = true;
+        trailEffect.enabled = true;
+
+        yield return new WaitForSeconds(0.3f);
+        meleeArea.enabled = false;
+
+        yield return new WaitForSeconds(0.3f);
+        trailEffect.enabled = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
