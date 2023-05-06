312d758cee09d43e33e80f1609e1f3ce94cbee8e
Neunggwon <104968524+Neunggwon@users.noreply.github.com>
2023-05-07 02:18:40 +0900

    Qaud Action-1.0.0-1
    Qaud Action-1.0.0-1

--- Assets/QuarterView 3D Action BE5/Scripts/Player.cs
+++ Assets/QuarterView 3D Action BE5/Scripts/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     
     public GameObject[] grenades;
     public int hasGrenades;
+    public Camera followCamera;
 
     public int ammo;
     public int coin;
@@ -26,6 +27,7 @@ public class Player : MonoBehaviour
     bool wDown;
     bool jDown;
     bool fDown;
+    bool rDown;
     bool iDown;
     bool sDown1;
     bool sDown2;
@@ -35,6 +37,7 @@ public class Player : MonoBehaviour
     bool isJump;
     bool isDodge;
     bool isSwap;
+    bool isReload;
     bool isFireReady = true;
 
     Vector3 moveVec;
@@ -68,6 +71,7 @@ public class Player : MonoBehaviour
         Turn();
         Jump();
         Attack();
+        Reload();
         Dodge();
         Interation();
         Swap();
@@ -79,7 +83,8 @@ public class Player : MonoBehaviour
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
         jDown = Input.GetButtonDown("Jump");
-        fDown = Input.GetButtonDown("Fire1");
+        fDown = Input.GetButton("Fire1");
+        rDown = Input.GetButtonDown("Reload");
         iDown = Input.GetButtonDown("Interation");
         sDown1 = Input.GetButtonDown("Swap1");
         sDown2 = Input.GetButtonDown("Swap2");
@@ -93,7 +98,7 @@ public class Player : MonoBehaviour
         {
             moveVec = dodgeVec;
         }
-        if (isSwap || !isFireReady)
+        if (isSwap || !isFireReady || isReload)
         {
             moveVec = Vector3.zero;
         }
@@ -106,7 +111,20 @@ public class Player : MonoBehaviour
 
     void Turn()
     {
+        //키보드에 의한 회전
         transform.LookAt(transform.position + moveVec);
+        //마우스에 의한 회전
+        if (fDown)
+        {
+            Ray ray = followCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rayHit;
+            if (Physics.Raycast(ray, out rayHit, 100))
+            {
+                Vector3 nextVec = rayHit.point - transform.position;
+                nextVec.y = 0;
+                transform.LookAt(transform.position + nextVec);
+            }
+        }
     }
 
     void Jump()
@@ -132,9 +150,39 @@ public class Player : MonoBehaviour
         if (fDown && isFireReady && !isDodge && !isSwap)
         {
             equipWeapon.Use();
-            anim.SetTrigger("DoSwing");
+            anim.SetTrigger(equipWeapon.type == Weapon.Type.Melee ? "DoSwing" : "DoShot");
             fireDelay = 0;
         }
+    }
+    void Reload()
+    {
+        if (equipWeapon == null)
+        {
+            return;
+        }
+        if (equipWeapon.type == Weapon.Type.Melee)
+        {
+            return;
+        }
+        if (ammo == 0)
+        {
+            return;
+        }
+        if (rDown && !isJump && !isDodge && !isSwap && isFireReady)
+        {
+            anim.SetTrigger("DoReload");
+            isReload = true;
+
+            Invoke("ReloadOut", 3f);
+        }
+    }
+
+    void ReloadOut()
+    {
+        int reAmmo = ammo < equipWeapon.maxAmmo ? ammo : equipWeapon.maxAmmo;
+        equipWeapon.curAmmo = reAmmo;
+        ammo -= reAmmo;
+        isReload = false;
     }
     void Dodge()
     {
--- Assets/QuarterView 3D Action BE5/Scripts/Weapon.cs
+++ Assets/QuarterView 3D Action BE5/Scripts/Weapon.cs
@@ -1,21 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Weapon : MonoBehaviour
 {
-    public enum Type { melee, Range };
+    public enum Type { Melee, Range };
     public Type type;
     public int damage;
     public float rate;
+    public int maxAmmo;
+    public int curAmmo;
+
     public BoxCollider meleeArea;
     public TrailRenderer trailEffect;
+    public Transform bulletPos;
+    public GameObject bullet;
+    public Transform bulletCasePos;
+    public GameObject bulletCase;
 
     public void Use()
     {
-        if (type == Type.melee)
+        if (type == Type.Melee)
         {
             StartCoroutine("Swing");
+        }
+        else if (type == Type.Range && curAmmo > 0)
+        {
+            curAmmo--;
+            StartCoroutine("Shot");
         }
     }
 
@@ -30,6 +43,21 @@ public class Weapon : MonoBehaviour
 
         yield return new WaitForSeconds(0.3f);
         trailEffect.enabled = false;
+    }
+    IEnumerator Shot()
+    {
+        // √—æÀ πﬂªÁ
+        GameObject IntantBullet = Instantiate(bullet, bulletPos.position, bulletPos.rotation);
+        Rigidbody bulletRigid = IntantBullet.GetComponent<Rigidbody>();
+        bulletRigid.velocity = bulletPos.forward * 50;
+        yield return null;
+
+        // ≈∫«« πË√‚
+        GameObject IntantCase = Instantiate(bulletCase, bulletCasePos.position, bulletCasePos.rotation);
+        Rigidbody caseRigid = IntantCase.GetComponent<Rigidbody>();
+        Vector3 caseVec = bulletCasePos.forward * Random.Range(-3, -2) + Vector3.up * Random.Range(2, 3);
+        caseRigid.AddForce(caseVec, ForceMode.Impulse);
+        caseRigid.AddTorque(Vector3.up * 10, ForceMode.Impulse);
     }
     // Start is called before the first frame update
     void Start()
